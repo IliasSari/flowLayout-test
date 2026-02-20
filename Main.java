fdea89e277cb59ba96698d8c0254b8f5755592f2
IliasSari <sarigilias@gmail.com>
2026-02-20 15:42:16 +0200

    added JLayeredPane

--- Main.java
+++ Main.java
@@ -1,19 +1,44 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 public class Main{
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame();
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0, 500, 500);
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.GREEN);
+        label2.setBounds(100,100,200,200);
+        
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.BLUE);
+        label3.setBounds(150,150,200,200);
+
+        layeredPane.add(label1);
+        layeredPane.add(label2);
+        layeredPane.add(label3);
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLayout(new GridLayout(3,3,10,10));
+        frame.setSize(new Dimension(500, 500));
+        frame.setLayout(null);
 
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(100,250));
@@ -28,7 +53,9 @@ public class Main{
         frame.add(new JButton("6"));
         frame.add(new JButton("7"));
         frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
+        frame.add(new JButton("9")); 
+
+        
 
         frame.add(panel);
         frame.setVisible(true);
